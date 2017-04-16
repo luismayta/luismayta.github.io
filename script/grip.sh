ef90e997fa96a17d3466c1537a507c778fd8d50f
@slovacus <slovacus@gmail.com>
2017-04-16 12:25:59 -0500

    !#3 Fix: update script shell

--- script/grip.sh
+++ script/grip.sh
@@ -3,3 +3,5 @@
 
 # shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+grip --pass "${GITHUB_API_TOKEN}" "${GRIP_PORT}"
--- script/lintcode.sh
+++ script/lintcode.sh
@@ -3,3 +3,6 @@
 
 # shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+pre-commit install -f --install-hooks
+pre-commit run --all-files
