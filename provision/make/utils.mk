0b29f55c7aa8d8b87cb859e9b5c979f523f9156b
Luis Mayta <luis@hadenlabs.com>
2019-11-15 16:41:18 -0500

    refactor: structure project (#15)

---
+++ provision/make/utils.mk
@@ -0,0 +1,23 @@
+#
+# See ./CONTRIBUTING.rst
+#
+.PHONY: utils.help
+
+utils.help:
+	@echo '    utils:'
+	@echo ''
+	@echo '        utils                      help utils'
+	@echo '        utils.generate             generate key ssh by stage'
+	@echo ''
+
+utils: clean
+	make utils.help
+
+utils.generate: clean
+	ssh-keygen -q -m PEM -t rsa -b 4096 -C "admin@${PROJECT}-${stage}.com" -f ${PROJECT}-${stage} -P ""
+	openssl rsa -in ${PROJECT}-${stage} -outform pem > ${PROJECT}-${stage}.pem
+	chmod 0600 ${PROJECT}-${stage}.pem
+	cp -rf ${PROJECT}-${stage}.pem ${KEYS_PEM_DIR}
+	cp -rf ${PROJECT}-${stage}.pub ${KEYS_PUB_DIR}
+	cp -rf ${PROJECT}-${stage}.pub ${KEYS_PRIVATE_DIR}
+	cp -rf ${PROJECT}-${stage} ${KEYS_PRIVATE_DIR}
