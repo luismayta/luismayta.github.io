71fc3feb049b863195ad61bec6d8a423f3ab6d95
@slovacus <slovacus@gmail.com>
2020-08-01 08:25:57 -0500

    refactor: actions for make python and yarn (#15)

--- provision/make/aws.mk
+++ provision/make/aws.mk
@@ -8,11 +8,15 @@ aws.help:
 	@echo '    Aws:'
 	@echo ''
 	@echo '        aws                    Run all help aws'
-	@echo '        aws.deploy             deploy files to bucket'
+	@echo '        aws.deploy             stage=(dev|prod) deploy files to bucket'
 	@echo ''
 
-aws: clean
+aws:
 	make aws.help
 
-aws.deploy: clean
-	$(AWS) s3 sync ${AWS_PATH_DEPLOY} ${AWS_BUCKET_NAME} --delete
+aws.deploy:
+	@if [ -z "${stage}" ] || [ "${stage}" == "dev" ]; then \
+		$(AWS) s3 sync ${AWS_PATH_DEPLOY} ${AWS_BUCKET_DEV_NAME} --delete; \
+	elif [ "${stage}" == "prod" ]; then \
+		$(AWS) s3 sync ${AWS_PATH_DEPLOY} ${AWS_BUCKET_PROD_NAME} --delete; \
+	fi
