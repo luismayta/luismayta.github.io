1dfa89ede656c8bdd2105019f88a4cf5b67fc35d
Luis Mayta <luis@hadenlabs.com>
2021-03-02 15:23:06 -0500

    chore: implement repository terraform (#42)

---
+++ provision/terraform/us-east-1/core/outputs.tf
@@ -0,0 +1,4 @@
+output "repository_luismayta_github_io_git_clone_url" {
+  description = "show url git of Luismayta github io"
+  value       = module.repository_luismayta_github_io.instance.git_clone_url
+}
