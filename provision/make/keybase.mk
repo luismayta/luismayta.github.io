b2cdc6f2be1cfe8e53a889bf372b858cef781f2f
Luis Mayta <luis@hadenlabs.com>
2021-03-02 18:22:41 -0500

    feat: implement path gpg (#44)

--- provision/make/keybase.mk
+++ provision/make/keybase.mk
@@ -16,10 +16,12 @@ keybase:
 
 keybase.environment:
 	@echo "=====> make environment for ${TEAM}..."
+	mkdir -p ${KEYBASE_PROJECT_PATH}/gpg
 	mkdir -p ${KEYBASE_PROJECT_PATH}/{staging,prod,dev,core}/{pem,private,password,pub,openssl}
 	@echo ${MESSAGE_HAPPY}
 
 keybase.setup:
 	@echo "=====> make dependences for ${TEAM}..."
+	mkdir -p ${KEYBASE_PROJECT_PATH}/gpg
 	mkdir -p ${KEYBASE_PROJECT_PATH}/{staging,prod,dev,core}/{pem,private,password,pub,openssl}
 	@echo ${MESSAGE_HAPPY}
